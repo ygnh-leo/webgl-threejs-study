cffb0f15af02273984c83657871631b3262d48ac
leo <ygnh136@qq.com>
2022-08-28 13:41:27 +0800

    feat: 对学习环境做了一些配置改变

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<div>
+			<header
+				className={'border-b border-r-gray-200 px-8 py-4 text-2xl font-bold'}
+			>
+				<Link href='/' className={'hover:text-amber-600'}>
+					webGL in Three.js
+				</Link>
+			</header>
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp
